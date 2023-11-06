02caa60e6f8abb5023e87a2e384fa313d359583c
AngelosGkelias <68152641+AngelosGkelias@users.noreply.github.com>
2023-11-06 18:56:14 +0200

    improved alg

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,5 +1,6 @@
 #Bubble Sort Implementation and Visualization using 
 array = []
+n = 0
 iterations = 0
 input_array = input("Please type your list seperated by commas:\n")
 array = input_array.split(sep= ",")
@@ -7,15 +8,17 @@ print(array)
 l = len(array)
 for j in range(l):
     array[j]=int(array[j])
+print(array)
 finished = False
 while finished == False:
     finished = True
-    for i in range(l-1):
+    for i in range(l-n-1):
         iterations += 1
         if array[i] > array[i+1]:
             
             array[i], array[i+1] = array[i+1], array[i]
             finished = False
+    n +=1
     
 print(array)
 print(iterations)
