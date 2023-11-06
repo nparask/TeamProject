24cdab53b77bfd0ef5a623e8393983be2327e393
AngelosGkelias <68152641+AngelosGkelias@users.noreply.github.com>
2023-11-06 19:10:10 +0200

    function

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,25 +1,27 @@
 #Bubble Sort Implementation and Visualization using 
-array = []
-n = 0
-iterations = 0
-input_array = input("Please type your list seperated by commas:\n")
-array = input_array.split(sep= ",")
-print(array)
-l = len(array)
-for j in range(l):
-    array[j]=int(array[j])
-print(array)
-finished = False
-while finished == False:
-    finished = True
-    for i in range(l-n-1):
-        iterations += 1
-        if array[i] > array[i+1]:
-            
-            array[i], array[i+1] = array[i+1], array[i]
-            finished = False
-    n +=1
-    
-print(array)
-print(iterations)
+
+def sorting_algorithm():
+    array = []
+    n = 0
+    global iterations
+    iterations = 0
+    input_array = input("Please type your list seperated by commas:\n")
+    array = input_array.split(sep= ",")
+    l = len(array)
+    for j in range(l):
+        array[j]=int(array[j])
+    finished = False
+    while finished == False:
+        finished = True
+        for i in range(l-n-1):
+            iterations += 1
+            if array[i] > array[i+1]:
+                
+                array[i], array[i+1] = array[i+1], array[i]
+                finished = False
+        n +=1
+    return array
+
+print("The sorted array is:", sorting_algorithm())
+print(f"It took {iterations} iterations.")
 end = input()
